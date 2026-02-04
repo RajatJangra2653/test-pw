3e9ceeb80f0371d766f3f8a7aab515194065c863
RajatJangra2653 <rajatjangra2653@gmail.com>
2026-02-04 09:50:56 +0530

    update

--- test.ps1
+++ test.ps1
@@ -1,1 +1,12 @@
-test
+Start-transcript -Path "C:\WindowsAzure\Logs\choco_install_log.txt" -Append
+
+
+$jobs = @(
+    Start-Job -ScriptBlock { choco install vscode -y -force }
+    Start-Job -ScriptBlock { choco install python311 -y -force }
+    Start-Job -ScriptBlock { choco install git -y -force }
+)
+
+Wait-Job -Job $jobs | Receive-Job
+
+Stop-Transcript
